b02643218b47deb2f5406fff3f38dfdca535297a
DaviChavesPinheiro <davichavespinheiro011001@gmail.com>
2021-07-30 15:52:39 -0300

    Icon and menu

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -1,7 +1,40 @@
 package src.frontend;
 
+import java.nio.file.Path;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("LSBS");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Change the icon image
+        Path cwd = Path.of("src/images/icon.png").toAbsolutePath();
+        ImageIcon img = new ImageIcon(cwd.toString());
+        frame.setIconImage(img.getImage());
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setOpaque(true);
+        menuBar.setBackground(new Color(29, 29, 29));
+        menuBar.setBorderPainted(false);
+        menuBar.setPreferredSize(new Dimension(1080, 20));
+        frame.setJMenuBar(menuBar);
+
+        //Display the window.
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Center
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
