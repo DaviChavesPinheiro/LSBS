62378adbec943cadb297da7b706c69bb04c06f20
DaviChavesPinheiro <davichavespinheiro011001@gmail.com>
2021-08-14 16:21:30 -0300

    Refactoring: AppView deleted

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 package src;
 
-import src.view.AppView;
+import javax.swing.JFrame;
+import src.view.ContentView;
+import src.view.SideBarView;
+import java.nio.file.Path;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     private static void createAndShowGUI() {
-        AppView.getInstance();
+        JFrame app = new JFrame("LSBS");
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Change the icon image
+        Path imgAbsPath = Path.of("src/images/icon.png").toAbsolutePath();
+        ImageIcon img = new ImageIcon(imgAbsPath.toString());
+        app.setIconImage(img.getImage());
+
+        // Create Main Panel
+        JPanel mPanel = new JPanel();
+        mPanel.setLayout(new BorderLayout());
+        mPanel.add(new SideBarView(), BorderLayout.LINE_START);
+        mPanel.add(new ContentView(), BorderLayout.CENTER);
+        
+        // Add Panel
+        app.setContentPane(mPanel);
+
+        //Display the window.
+        app.pack();
+        app.setResizable(false);
+        app.setLocationRelativeTo(null); // Center
+        app.setVisible(true);
     }
 
     public static void main(String[] args) {
