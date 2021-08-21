44686a010a575a685a32756f20859c3a19134a3f
DaviChavesPinheiro <davichavespinheiro011001@gmail.com>
2021-08-21 09:23:06 -0300

    Refactoring: sidebar margin

--- src/view/SideBar/SideBarView.java
+++ src/view/SideBar/SideBarView.java
@@ -11,6 +11,10 @@ public class SideBarView extends JPanel  {
         this.setOpaque(true);
         this.setBackground(new Color(29, 29, 29));
         this.setPreferredSize(new Dimension(1080 / 4 * 1, 720));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // Margin
+        this.add(Box.createRigidArea(new Dimension(0, 35)));
 
         this.add(new DonutView());
     }
